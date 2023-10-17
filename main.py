cc2fc9a010b8e832152f14b5f52a0fc8715922c8
Alvaro Infante <Alvaroinfantee@gmail.com>
2023-10-17 11:54:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 database = {
     "Joe": {
         "balance": 1000,
-        "face_encoding": face_recognition.face_encodings(face_recognition.load_image_file(r"C:\Users\alvar\Downloads\rogan1.jpg"))[0]
+        "face_encoding": face_recognition.face_encodings(face_recognition.load_image_file('rogan1.jpeg'))[0]
     },
     "Dillon": {
         "balance": 1000,
